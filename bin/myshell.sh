d469b6bff5ec97a739bb97ca4931c02a37ed212f
Alan Dong <alandong@stanford.edu>
2020-02-19 16:42:38 -0800

    add or

--- bin/myshell.sh
+++ bin/myshell.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 for i in {1..5}; do
-  if [ $i != 3 ]; then
-    echo "Welcome $i times"
-  else
-    echo "failed and stop"
-    exit 1
-  fi
+  echo "Welcome $i times"
+
+  # if [ $i != 3 ]; then
+  #   echo "Welcome $i times"
+  # else
+  #   echo "failed and stop"
+  #   exit 1
+  # fi
 done
 
